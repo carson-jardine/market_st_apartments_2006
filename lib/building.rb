0b033cc1c50b644273b18afbda51dea1d7625fd2
Carson Jardine <carsonjardine@gmail.com>
2020-07-14 10:19:21 -0600

    Add more tests and methods

--- lib/building.rb
+++ lib/building.rb
@@ -1,9 +1,23 @@
 class Building
 
-  attr_reader :units 
+  attr_reader :units, :renters
 
   def initialize
     @units = []
+    @renters = renters
+  end
+
+  def add_unit(unit)
+    @units << unit
+  end
+
+  def renters
+    @renters = []
+    @units.each do |unit|
+      unit.renter == renter
+        @renters << renter
+    end
+    @renters
   end
 
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -18,31 +18,35 @@ class BuildingTest < Minitest::Test
     assert_equal [], @building.units
   end
 
-  #
-  # pry(main)> unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-  # # => #<Apartment:0x00007f8377209bb0...>
-  #
-  # pry(main)> unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-  # # => #<Apartment:0x00007f83779f0900...>
-  #
-  # pry(main)> building.add_unit(unit1)
-  #
-  # pry(main)> building.add_unit(unit2)
-  #
-  # pry(main)> building.units
-  # # => [#<Apartment:0x00007f8377209bb0...>, #<Apartment:0x00007f83779f0900...>]
-  #
-  # pry(main)> building.renters
-  # # => []
-  #
-  # pry(main)> renter1 = Renter.new("Aurora")
-  # # => #<Renter:0x00007fa83bc37978...>
-  #
-  # pry(main)> unit1.add_renter(renter1)
-  #
-  # pry(main)> building.renters
-  # # => ["Aurora"]
-  #
+  def test_building_can_add_units
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @building.add_unit(unit1)
+    @building.add_unit(unit2)
+
+    assert_equal [unit1, unit2], @building.units
+  end
+
+  def test_building_can_add_renters
+    skip
+    assert_equal [], @building.renters
+
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @building.add_unit(unit1)
+    @building.add_unit(unit2)
+
+    renter1 = Renter.new("Aurora")
+    unit1.add_renter(renter1)
+
+    assert_equal ["Aurora"], @building.renters
+
+    renter2 = Renter.new("Tim")
+    unit2.add_renter(renter2)
+
+    assert_equal ["Aurora", "Tim"], @building.renters
+  end
+
   # pry(main)> renter2 = Renter.new("Tim")
   # # => #<Renter:0x00007fa83b9b0358...>
   #
@@ -50,7 +54,20 @@ class BuildingTest < Minitest::Test
   #
   # pry(main)> building.renters
   # # => ["Aurora", "Tim"]
-  #
+
+  def test_average_rent
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @building.add_unit(unit1)
+    @building.add_unit(unit2)
+    renter1 = Renter.new("Aurora")
+    unit1.add_renter(renter1)
+    renter2 = Renter.new("Tim")
+    unit2.add_renter(renter2)
+
+    assert_equal 1099.5, @building.average_rent
+  end
+
   # pry(main)> building.average_rent
   # # => 1099.5
 
